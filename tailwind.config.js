3d3ea9a1c7eec114b74921b8a997f5ff67d03e0c
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2024-09-16 21:17:50 +0000

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './budget/**/*.{html, js}',
+    './node_modules/flowbite/**/*.js'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
